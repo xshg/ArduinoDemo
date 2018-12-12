8e6d0ed92e4729121857a27c93f23365b5780b77
xshg <xshg@msn.com>
2018-12-12 15:20:13 +0800

    andd serialControlLED.ino

---
+++ serialControlLED/serialControlLED.ino
@@ -0,0 +1,29 @@
+int timeDelay = 1000;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (Serial.available()>0){
+    char c = Serial.read();
+    Serial.println(c);
+    if (c=='a'){
+      timeDelay=100;
+    }
+    else if(c=='b'){
+      timeDelay=500;
+    }
+    else if(c=='c'){
+      timeDelay=1000;
+    }
+  }
+  
+  digitalWrite(13, HIGH);
+  delay(timeDelay);
+  digitalWrite(13, LOW);
+  delay(timeDelay);
+  
+}
